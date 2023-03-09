28b810783681f1e857e43493609fe637a9fefddb
AnhThi <ahthi2903@gmail.com>
2023-03-09 15:58:44 +0700

    modified:   Chuong2/Bai2.2.py 	modified:   Chuong2/Bai2.3.py

--- Chuong2/Bai2.2.py
+++ Chuong2/Bai2.2.py
@@ -2,5 +2,5 @@ r=int(input('Nhap vao ban kinh cua duong tron: '))
 pi=3.14
 s=r*r*pi
 c=2*r*pi
-print('Dien tich cua duong tron co ban kinh ',r,' la = '+str(round(s,1)))
-print('Chu vi cua duong tron co ban kinh ',r ,' la = '+str(round(c,1)))
+print('Dien tich cua duong tron co ban kinh',r,'la = '+str(round(s,1)))
+print('Chu vi cua duong tron co ban kinh',r,'la = '+str(round(c,1)))
